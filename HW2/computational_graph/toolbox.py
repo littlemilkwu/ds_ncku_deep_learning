fixed: double softmax issue

HW2/computational_graph/toolbox.py
@@ -16,7 +16,7 @@ def read_img_feature_data(part='train'):
     data = np.load(f"{DATA_PATH}/../code/output/{part}_features.npz")
     return data['X'], data['y']
 
-def one_hot_encoding(y):
-    y_one = np.zeros((y.size, y.max() + 1))
+def one_hot_encoding(y, class_num):
+    y_one = np.zeros((y.size, class_num))
     y_one[np.arange(y.size), y] = 1
     return y_one

HW2/lenet_from_strach/lenet_train.py
@@ -12,13 +12,16 @@ from toolbox import *
 
 parser = ArgumentParser()
 parser.add_argument("-v", "--version", default='origin', type=str)
-parser.add_argument("-e", "--epoch", default=EPOCHS, type=int)
+parser.add_argument("-e", "--epochs", default=EPOCHS, type=int)
 parser.add_argument("-b", "--batch_size", default=BATCH_SIZE, type=int)
+parser.add_argument("-m", "--mode", default='official', type=str)
 args = parser.parse_args()
 
 EPOCHS = args.epochs
 BATCH_SIZE = args.batch_size
 VERSION = args.version
+MODE = args.mode
+dict_hyper = {"v": VERSION, "e": EPOCHS, "b": BATCH_SIZE}
 
 def preprocessing():
     train_X, train_y = read_pixel_data(part='train')
@@ -26,9 +29,14 @@ def preprocessing():
     train_X, val_X = train_X / float(255), val_X / float(255)
     train_X -= np.mean(train_X)
     val_X -= np.mean(val_X)
-    train_y = one_hot_encoding(train_y)
-    val_y = one_hot_encoding(val_y)
+    train_y = one_hot_encoding(train_y, 50)
+    val_y = one_hot_encoding(val_y, 50)
     return (train_X, train_y), (val_X, val_y)
+
+def test_mode(train_X, train_y):
+    train_X, train_y = union_shuffle(train_X, train_y)
+    train_X, train_y = train_X[:10000], train_y[:10000]
+    return train_X, train_y
 
 def train_loop(model, train_X, train_y, val_X, val_y, loss_fn, optim):
     ls_loss = []
@@ -57,7 +65,7 @@ def train_loop(model, train_X, train_y, val_X, val_y, loss_fn, optim):
 
                 if val_loss < best_val_loss:
                     best_val_loss = val_loss
-                    save_model(model)
+                    save_model(model, dict_hyper)
 
         train_loss /= batch_cnt
         train_acc /= batch_cnt
@@ -65,42 +73,25 @@ def train_loop(model, train_X, train_y, val_X, val_y, loss_fn, optim):
     return ls_loss
 
 def main():
+    global VERSION, MODE, EPOCHS, BATCH_SIZE, LEARNING_RATE
     s_time = time.time()
+    
     (train_X, train_y), (val_X, val_y) = preprocessing()
     if VERSION == 'improved':
         model = LeNet5Imp()
     elif VERSION == 'origin':
         model = LeNet5()
-    optim = SGDMomentum(model.get_params(), lr=1e-3, momentum=0.80, reg=0.00003)
+
+    if MODE == "test":
+        train_X, train_y = test_mode(train_X, train_y)
+        VERSION = VERSION + '_test'
+    
+    optim = SGDMomentum(model.get_params(), lr=LEARNING_RATE, momentum=0.80, reg=0.00003)
     loss_fn = CrossEntropyLoss()
     ls_loss = train_loop(model, train_X, train_y, val_X, val_y, loss_fn, optim)
 
-    dict_hyper = {"v": VERSION, "e": EPOCHS, "b": BATCH_SIZE}
     save_loss(ls_loss, dict_hyper)
-    save_model(model, dict_hyper)
-
     print(f'total spent: {int(time.time() - s_time)} secs.')
-    # time_start = time.time()
-
-    # train_X = train_X[:BATCH_SIZE]
-    # train_y = train_y[:BATCH_SIZE]
-    
-    # y_pred = model.forward(train_X)
-    # print(f'forward used: {time.time() - time_start}')
-    # loss, dout = loss_fn.get(y_pred, train_y)
-    # model.backward(dout)
-    # optim.step()
-
-    # time_end = time.time()
-    # print(f'one batch spend {round(time_end - time_start, 2)}')
-def test():
-    conv1 = Conv(1, 6, 5)
-    np_ar1 = np.arange(98).reshape(2, 1, 7, 7)
-    print('ar1: ', np_ar1)
-
-    np_ar2 = conv1._forward(np_ar1)
-    # print('ar2: ', np_ar2)
 
 if __name__ == '__main__':
-    # test()
     main()

HW2/lenet_from_strach/model_layer.py
@@ -71,6 +71,27 @@ class Sigmoid():
         dX = dout*X*(1-X)
         return dX
 
+class SigmoidImp():
+    """
+    Sigmoid activation layer
+    """
+    def __init__(self):
+        self.cache = None
+
+    def _forward(self, X):
+        X = np.longdouble(X)
+        self.cache = X
+        self.sig_cache = self.origin_sig(X)
+        return self.sig_cache * X
+    
+    def origin_sig(self, X):
+        return (1 / (1 + np.exp(-X)))
+
+    def _backward(self, dout):
+        sig_dev = (self.sig_cache)*(1-self.sig_cache)
+        dX = dout * (self.sig_cache + sig_dev*self.cache)
+        return dX
+
 class tanh():
     """
     tanh activation layer
@@ -282,8 +303,11 @@ class CrossEntropyLoss():
 
     def get(self, Y_pred, Y_true):
         N = Y_pred.shape[0]
-        softmax = Softmax()
-        prob = softmax._forward(Y_pred)
+
+        # softmax = Softmax()
+        # prob = softmax._forward(Y_pred) # double softmax
+        
+        prob = Y_pred
         loss = NLLLoss(prob, Y_true)
         Y_serial = np.argmax(Y_true, axis=1)
         dout = prob.copy()
@@ -461,29 +485,36 @@ class LeNet5Imp(Net):
     # LeNet5 Improved
 
     def __init__(self):
-        self.conv1 = Conv(3, 6, F=3)
-        self.Sig1 = Sigmoid()
+        self.conv1_1 = Conv(3, 6, F=3)
+        self.Sig1_1 = SigmoidImp()
+        self.conv1_2 = Conv(6, 6, F=3)
+        self.Sig1_2 = SigmoidImp()
         self.pool1 = MaxPool(2, 2)
         self.conv2 = Conv(6, 16, F=3)
-        self.Sig2 = Sigmoid()
+        self.Sig2 = SigmoidImp()
         self.pool2 = MaxPool(2, 2)
+
         self.FC1 = FC(16 * 6 * 6, 120)
-        self.Sig3 = Sigmoid()
+        self.Sig3 = SigmoidImp()
         self.FC2 = FC(120, 84)
-        self.Sig4 = Sigmoid()
+        self.Sig4 = SigmoidImp()
         self.FC3 = FC(84, 50)
         self.Softmax = Softmax()
 
         self.p2_shape = None
 
     def forward(self, X):
-        h1 = self.conv1._forward(X)
-        a1 = self.Sig1._forward(h1)
-        p1 = self.pool1._forward(a1)
+        h1_1 = self.conv1_1._forward(X)
+        a1_1 = self.Sig1_1._forward(h1_1)
+        h1_2 = self.conv1_2._forward(a1_1)
+        a1_2 = self.Sig1_2._forward(h1_2)
+
+        p1 = self.pool1._forward(a1_2)
         h2 = self.conv2._forward(p1)
         a2 = self.Sig2._forward(h2)
         p2 = self.pool2._forward(a2)
         self.p2_shape = p2.shape
+
         fl = p2.reshape(X.shape[0], -1) # Flatten
         h3 = self.FC1._forward(fl)
         a3 = self.Sig3._forward(h3)
@@ -505,14 +536,16 @@ class LeNet5Imp(Net):
         dout = self.Sig2._backward(dout)
         dout = self.conv2._backward(dout)
         dout = self.pool1._backward(dout)
-        dout = self.Sig1._backward(dout)
-        dout = self.conv1._backward(dout)
+        dout = self.Sig1_2._backward(dout)
+        dout = self.conv1_2._backward(dout)
+        dout = self.Sig1_1._backward(dout)
+        dout = self.conv1_1._backward(dout)
 
     def get_params(self):
-        return [self.conv1.W, self.conv1.b, self.conv2.W, self.conv2.b, self.FC1.W, self.FC1.b, self.FC2.W, self.FC2.b, self.FC3.W, self.FC3.b]
+        return [self.conv1_1.W, self.conv1_1.b, self.conv1_2.W, self.conv1_2.b, self.conv2.W, self.conv2.b, self.FC1.W, self.FC1.b, self.FC2.W, self.FC2.b, self.FC3.W, self.FC3.b]
 
     def set_params(self, params):
-        [self.conv1.W, self.conv1.b, self.conv2.W, self.conv2.b, self.FC1.W, self.FC1.b, self.FC2.W, self.FC2.b, self.FC3.W, self.FC3.b] = params
+        [self.conv1_1.W, self.conv1_1.b, self.conv1_2.W, self.conv1_2.b, self.conv2.W, self.conv2.b, self.FC1.W, self.FC1.b, self.FC2.W, self.FC2.b, self.FC3.W, self.FC3.b] = params
 
 
 class SGD():

HW2/lenet_from_strach/toolbox.py
@@ -9,7 +9,6 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 from multiprocessing import Pool, cpu_count
 
-
 from config import *
 
 def union_shuffle(X, y):
@@ -40,8 +39,8 @@ def read_pixel_data(part="train"):
         y = np.array(y, dtype=int)
     return (X, y)
 
-def one_hot_encoding(y):
-    y_one = np.zeros((y.size, y.max() + 1))
+def one_hot_encoding(y, class_num):
+    y_one = np.zeros((y.size, class_num))
     y_one[np.arange(y.size), y] = 1
     return y_one
 
@@ -51,8 +50,27 @@ def save_loss(ls_loss, dict_hyper:dict):
 
 def save_model(model, dict_hyper:dict):
     weights = model.get_params()
-    with open(f"output/lenet_{dict_hyper['v']}_e{dict_hyper['e']}_b{dict_hyper['b']}_weights.pkl","wb") as f:
+    with open(f"model_weights/lenet_{dict_hyper['v']}_e{dict_hyper['e']}_b{dict_hyper['b']}_weights.pkl","wb") as f:
         pickle.dump(weights, f)
+
+def draw_loss_n_save(ls_loss, dict_hyper):
+    train_loss, train_acc, val_loss, val_acc = zip(*ls_loss)
+    epochs = list(range(1, len(train_loss) + 1) )
+    fig, ax = plt.subplots(2, 1, figsize=(10, 6))
+
+    fig.suptitle(f"{dict_hyper['v']} lenet e{dict_hyper['e']} b{dict_hyper['b']}")
+    ax[0].plot(epochs, train_loss, label='train_loss')
+    ax[0].plot(epochs, val_loss, label='val_loss')
+    ax[0].set_xticks(epochs)
+    ax[0].legend()
+
+    ax[1].plot(epochs, train_acc, label='train_acc')
+    ax[1].plot(epochs, val_acc, label='val_acc')
+    ax[1].set_xticks(epochs)
+    ax[1].legend()
+    ax[1].set_xlabel('Epochs')
+    plt.tight_layout()
+    fig.savefig('test.png')
 
 # above by myself
 